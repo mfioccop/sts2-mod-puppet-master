34de9ac780b21e096c94711b588edf57915571df
Mallory Fioccoprile <mfioccoprile@gmail.com>
2026-08-09 20:35:36 -0700

    feat: add TwistedStrings Ancient card upgrade for StringAlong

--- PuppetMasterCode/Cards/Ancient/Attacks/TwistedStrings.cs
+++ PuppetMasterCode/Cards/Ancient/Attacks/TwistedStrings.cs
@@ -1,30 +1,29 @@
-using BaseLib.Abstracts;
 using BaseLib.Utils;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
-using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Models.Powers;
 using MegaCrit.Sts2.Core.ValueProps;
-using PuppetMaster.PuppetMasterCode.Cards.Ancient.Attacks;
 using PuppetMaster.PuppetMasterCode.Vars;
 
-namespace PuppetMaster.PuppetMasterCode.Cards.Basic.Attacks;
+namespace PuppetMaster.PuppetMasterCode.Cards.Ancient.Attacks;
 
-public class StringAlong() : PuppetMasterCard(1, CardType.Attack, CardRarity.Basic, TargetType.AnyEnemy), ITranscendenceCard
+public class TwistedStrings() : PuppetMasterCard(1, CardType.Attack, CardRarity.Ancient, TargetType.AnyEnemy)
 {
     protected override IEnumerable<DynamicVar> CanonicalVars =>
     [
-        new DamageVar(3, ValueProp.Move),
-        new RestringVar(3),
+        new DamageVar(7, ValueProp.Move),
+        new RestringVar(2),
         new PowerVar<StrengthPower>(1),
+        new PowerVar<DexterityPower>(1),
     ];
 
     protected override IEnumerable<IHoverTip> ExtraHoverTips =>
     [
         ..base.ExtraHoverTips,
         HoverTipFactory.FromPower<StrengthPower>(),
+        HoverTipFactory.FromPower<DexterityPower>(),
     ];
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay play)
@@ -39,6 +38,7 @@ public class StringAlong() : PuppetMasterCard(1, CardType.Attack, CardRarity.Bas
         if (await TryRestring(choiceContext, play.Target) > 0)
         {
             await CommonActions.Apply<StrengthPower>(choiceContext, play.Target, this, -DynamicVars.Strength.BaseValue);
+            await CommonActions.Apply<DexterityPower>(choiceContext, play.Target, this, -DynamicVars.Dexterity.BaseValue);
         }
     }
 
@@ -46,6 +46,4 @@ public class StringAlong() : PuppetMasterCard(1, CardType.Attack, CardRarity.Bas
     {
         EnergyCost.UpgradeBy(-1);
     }
-
-    public CardModel GetTranscendenceTransformedCard() => ModelDb.Card<TwistedStrings>();
 }
