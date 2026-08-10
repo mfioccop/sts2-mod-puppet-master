725e56a76bcff459e7b6a98c504577d41db37781
Mallory Fioccoprile <mfioccoprile@gmail.com>
2026-08-09 21:17:39 -0700

    feat: add CurtainCall

---
+++ PuppetMasterCode/Cards/Rare/Skills/CurtainCall.cs
@@ -0,0 +1,38 @@
+using BaseLib.Extensions;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+using PuppetMaster.PuppetMasterCode.Vars;
+
+namespace PuppetMaster.PuppetMasterCode.Cards.Rare.Skills;
+
+public class CurtainCall() : PuppetMasterCard(0, CardType.Skill, CardRarity.Rare, TargetType.AnyEnemy)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new BlockVar(6, ValueProp.Move),
+        new RestringVar(2),
+        new RepeatVar(1).WithUpgrade(1),
+    ];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay play)
+    {
+        if (play.Target == null)
+        {
+            return;
+        }
+
+        var repeat = 1;
+        if (await TryRestring(choiceContext, play.Target) > 0)
+        {
+            repeat += DynamicVars.Repeat.IntValue;
+        }
+
+        for (var i = 0; i < repeat; i++)
+        {
+            await CreatureCmd.GainBlock(Owner.Creature, DynamicVars.Block, play);
+        }
+    }
+}
