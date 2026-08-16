d615d1c161eff50a3aa47fe3a2072302f1412f20
Mallory Fioccoprile <mfioccoprile@gmail.com>
2026-08-16 00:00:30 -0700

    feat: add FinalTouches

---
+++ PuppetMasterCode/Cards/Common/Skills/FinalTouches.cs
@@ -0,0 +1,32 @@
+using BaseLib.Extensions;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+using PuppetMaster.PuppetMasterCode.Vars;
+
+namespace PuppetMaster.PuppetMasterCode.Cards.Common.Skills;
+
+public class FinalTouches() : PuppetMasterCard(1, CardType.Skill, CardRarity.Common, TargetType.AnyEnemy)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new BlockVar(5, ValueProp.Move).WithUpgrade(2),
+        new RestringVar(2),
+    ];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay play)
+    {
+        await CommonActions.CardBlock(this, play);
+        if (await TryRestring(choiceContext, play.Target) > 0)
+        {
+            var card = await CommonActions.SelectSingleCard(this, SelectionScreenPrompt, choiceContext, PileType.Discard);
+            if (card != null)
+            {
+                await CardPileCmd.Add(card, PileType.Hand);
+            }
+        }
+    }
+}
