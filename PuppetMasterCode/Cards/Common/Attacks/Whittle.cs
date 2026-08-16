7f4bd2dd10b92afb922dcebc169942d9fe7cae22
Mallory Fioccoprile <mfioccoprile@gmail.com>
2026-08-16 00:53:13 -0700

    feat: add Whittle

---
+++ PuppetMasterCode/Cards/Common/Attacks/Whittle.cs
@@ -0,0 +1,22 @@
+using BaseLib.Extensions;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace PuppetMaster.PuppetMasterCode.Cards.Common.Attacks;
+
+public class Whittle() : PuppetMasterCard(1, CardType.Attack, CardRarity.Common, TargetType.AnyEnemy)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new DamageVar(3, ValueProp.Move),
+        new RepeatVar(3).WithUpgrade(1),
+    ];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay play)
+    {
+        await CommonActions.CardAttack(this, play, DynamicVars.Repeat.IntValue).Execute(choiceContext);
+    }
+}
