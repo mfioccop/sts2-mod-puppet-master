89512a09a62b0ac135518234127a61199ef45235
Mallory Fioccoprile <mfioccoprile@gmail.com>
2026-08-17 14:50:21 -0700

    feat: add StringExtensions#PotionImagePath and fallback potion image, add override PuppetMasterPotion#CustomPackedImagePath
    The mod template didn't have an image helper for potions like the rest of the models; this patch was added in a recent update of the template, but since this project was created before it didn't support potion images.

--- PuppetMasterCode/Extensions/StringExtensions.cs
+++ PuppetMasterCode/Extensions/StringExtensions.cs
@@ -63,6 +63,15 @@ public static class StringExtensions
         MainFile.Logger.Info("Could not find big relic image path: " + path);
         return Path.Join(MainFile.ResPath, "images", "relics", "big", "relic.png");
     }
+    
+    public static string PotionImagePath(this string path)
+    {
+        path = Path.Join(MainFile.ResPath, "images", "potions", path);
+        if (ResourceLoader.Exists(path)) return path;
+
+        MainFile.Logger.Info("Could not find potion image path: " + path);
+        return Path.Join(MainFile.ResPath, "images", "potions", "potion.png");
+    }
 
     public static string CharacterUiPath(this string path)
     {
--- PuppetMasterCode/Potions/PuppetMasterPotion.cs
+++ PuppetMasterCode/Potions/PuppetMasterPotion.cs
@@ -1,8 +1,13 @@
 ﻿using BaseLib.Abstracts;
+using BaseLib.Extensions;
 using BaseLib.Utils;
 using PuppetMaster.PuppetMasterCode.Character;
+using PuppetMaster.PuppetMasterCode.Extensions;
 
 namespace PuppetMaster.PuppetMasterCode.Potions;
 
 [Pool(typeof(PuppetMasterPotionPool))]
-public abstract class PuppetMasterPotion : CustomPotionModel;
+public abstract class PuppetMasterPotion : CustomPotionModel
+{
+    public override string? CustomPackedImagePath => $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".PotionImagePath();
+}
